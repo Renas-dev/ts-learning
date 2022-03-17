611758f0baf2dad44adfb17cd3e4256d095b4a57
Renask <renask.dev@gmail.com>
2022-03-17 13:09:01 +0100

    Learned the types that TS uses

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -23,3 +23,17 @@ function doSomething(){
 
 doSomething(); */
 
+/*lesson 3, Types  
+let a: number; //any integer or floating point numbers
+let b: boolean; //true or false
+let c: string; //letters or words
+let d: any; // can be used to store any type
+let e: number[] = [1, 2, 3,]; //arrays
+let f: any[] = [1, true, 'a', false]// any can be used for array's too but its bad practice and avoid this
+
+enum Color {Red = 0, Green = 1, Blue = 2, Purple = 3 };
+let backgroundColor = Color.Red; */
+
+
+
+
